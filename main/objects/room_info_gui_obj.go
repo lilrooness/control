23c9a5f73d680e1927ee2b1e4593b47180fa10dc
lilrooness <jfrangoudes@gmail.com>
2018-12-12 21:58:03 +0000

    Use monarch to control screen transitions

--- main/objects/room_info_gui_obj.go
+++ main/objects/room_info_gui_obj.go
@@ -13,18 +13,3 @@ components {
     w: 1.0
   }
 }
-components {
-  id: "room_info_gui_obj"
-  component: "/main/scripts/room_info_proxy_obj.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
