53762e9ffc0ae4a1415dfbb19fa2f79bd4c43640
Joe Frangoudes <jfrangoudes@gmail.com>
2019-01-06 23:42:42 +0000

    Map script passes level events to level script via map_state module

--- main/modules/map_state.lua
+++ main/modules/map_state.lua
@@ -24,7 +24,9 @@ local state = {
 		"i",
 		"j",
 		"k"
-	}
+	},
+
+	level_script_url = nil
 }
 
 function M.get_display_id(n)
@@ -45,6 +47,25 @@ end
 
 function M.rooms_selected()
 	return state.rooms
+end
+
+function M.set_level_script(url)
+	state.level_script_url = url
+end
+
+function M.level_event(event_code, event)
+	-- during level init, this might be nil but door events can still fire
+	if state.level_script_url ~= nil then
+		msg.post(state.level_script_url, event_code, event)
+	end
+end
+
+function M.door_opened_event(door_id)
+	M.level_event("door_opened", {door_id = door_id})
+end
+
+function M.door_closed_event(door_id)
+	M.level_event("door_closed", {door_id = door_id})
 end
 
 return M
