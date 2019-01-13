63cc9353d114ea79a7a0fcafe20cfefcb4eb24fd
Joe Frangoudes <jfrangoudes@gmail.com>
2019-01-13 14:33:54 +0000

    Malfunction alarm warning displays when Emily reaches room 5

---
+++ main/objects/malfunction_alarm_gui_obj.go
@@ -0,0 +1,15 @@
+components {
+  id: "malfunction_alert"
+  component: "/main/guis/malfunction_alert.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
