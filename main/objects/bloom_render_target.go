995d2b03e8c5d339c29c328be9cc20460dbbad5e
Joe Frangoudes <jfrangoudes@gmail.com>
2019-01-14 21:56:07 +0000

    scanline shader

--- main/objects/bloom_render_target.go
+++ main/objects/bloom_render_target.go
@@ -1,8 +1,23 @@
+components {
+  id: "scanline_gfx"
+  component: "/main/scripts/scanline_gfx.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "model"
   type: "model"
   data: "mesh: \"/assets/quad.dae\"\n"
-  "material: \"/assets/materials/bloom_mat/bloom.material\"\n"
+  "material: \"/assets/materials/scanlines/scanlines.material\"\n"
   "skeleton: \"\"\n"
   "animations: \"\"\n"
   "default_animation: \"\"\n"
