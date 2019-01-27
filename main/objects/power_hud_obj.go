726ff51da75cc227746599a6c34a48de3599b77e
JFrangoudes <lilrooness@users.noreply.github.com>
2019-01-27 23:04:02 +0000

    Power bar when in [P]ower mode

---
+++ main/objects/power_hud_obj.go
@@ -0,0 +1,15 @@
+components {
+  id: "power_gui"
+  component: "/main/guis/power_gui.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
