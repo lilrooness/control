05cde46729124f5ad927641597a307013d67f6d4
JFrangoudes <lilrooness@users.noreply.github.com>
2019-01-27 21:18:21 +0000

    power button displayed in HUD

---
+++ main/objects/sound.go
@@ -0,0 +1,115 @@
+components {
+  id: "sound_fade"
+  component: "/main/scripts/sound_fade.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "440_660"
+  type: "sound"
+  data: "sound: \"/assets/sfx/menu/440_660.wav\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 0.15\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "660_440"
+  type: "sound"
+  data: "sound: \"/assets/sfx/menu/660_440.wav\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 0.15\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "440"
+  type: "sound"
+  data: "sound: \"/assets/sfx/menu/440.wav\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 0.15\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "660"
+  type: "sound"
+  data: "sound: \"/assets/sfx/menu/660.wav\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 0.15\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sound"
+  type: "sound"
+  data: "sound: \"/assets/sfx/fridge-ambiance_downsampled.wav\"\n"
+  "looping: 1\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
