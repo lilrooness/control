56e18b69a96439f2536ede33b043ace58581f296
JFrangoudes <lilrooness@users.noreply.github.com>
2019-05-08 19:09:54 +0100

    Dialogue boxes work

--- main/objects/sound.go
+++ main/objects/sound.go
@@ -113,3 +113,23 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "static_blip"
+  type: "sound"
+  data: "sound: \"/assets/sfx/radio_static_blip.wav\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
